3e27733bd2c6a765259e723e8fb83c5f017fd1ae
Tim Park <timsjpark@gmail.com>
2015-11-21 17:30:40 -0700

    Add create method to OrganizationsController
    [#108686632]

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,4 +2,24 @@ class OrganizationsController < ApplicationController
   def new
     @organization = Organization.new
   end
+
+  def create
+    @organization = Organization.new(organization_params)
+
+    if @organization.save
+      redirect_to employees_path,
+        notice: "Thank you for signing up!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def organization_params
+    params.require(:organization).permit(
+      :name, :address, :city, :state, :zip_code, :phone_number,
+      :email, :password, :password_confirmation
+    )
+  end
 end
